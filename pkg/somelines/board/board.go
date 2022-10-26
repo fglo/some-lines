61997bac7e68d5225220085db9145cd0a48d8634
fglo <fmglowacki@gmail.com>
2022-10-26 10:58:16 +0200

    refactored drawing polygons

--- pkg/somelines/board/board.go
+++ pkg/somelines/board/board.go
@@ -4,6 +4,7 @@ import (
 	"math"
 
 	"github.com/fglo/some-lines/pkg/somelines/point"
+	"github.com/fglo/some-lines/pkg/somelines/shapes"
 )
 
 // Board encapsulates simulation logic
@@ -48,25 +49,23 @@ func (b *Board) Size() (w, h int) {
 // Draw draws board
 func (b *Board) Draw(pixels []byte, counter, focalLength int) {
 	theta := float64((2*counter)%360) * math.Pi / 180.0
-	// b.RotateTriangle(point.NewPoint2D(30, 30), point.NewPoint2D(60, 25), point.NewPoint2D(40, 50), theta, pixels)
-	b.RotateQuadrangle(point.NewPoint2D(30, 30), point.NewPoint2D(60, 25), point.NewPoint2D(70, 50), point.NewPoint2D(40, 50), theta, pixels)
 
-	vertices := []*point.Point2D{point.NewPoint2D(30, 70), point.NewPoint2D(60, 65), point.NewPoint2D(70, 90), point.NewPoint2D(40, 90)}
-	edges := [][2]int{{0, 1}, {1, 2}, {2, 3}, {3, 0}}
-	b.RotatePolygon(vertices, edges, -theta, pixels)
+	b.DrawPolygon(shapes.NewTriangle(point.NewPoint2D(30, 30), point.NewPoint2D(60, 25), point.NewPoint2D(70, 50)).Rotate(theta), pixels)
+
+	b.DrawPolygon(shapes.NewQuadrangle(point.NewPoint2D(30, 70), point.NewPoint2D(60, 65), point.NewPoint2D(70, 90), point.NewPoint2D(40, 90)).Rotate(-theta), pixels)
 
 	b.DrawLine(point.NewPoint2D(30, 120), point.NewPoint2D(70, 130), pixels)
 	b.DrawLine3DRelativeToPoint(point.NewPoint3D(30, 140, 0), point.NewPoint3D(70, 150, 20), point.NewPoint2D(30, 140), focalLength, pixels)
 	b.DrawLine3DRelativeToPoint(point.NewPoint3D(30, 160, 10), point.NewPoint3D(70, 175, 20), point.NewPoint2D(55, 160), focalLength, pixels)
 
-	p1 := point.NewPoint3D(80, 20, 0)
-	p2 := point.NewPoint3D(100, 40, 0)
-	p3 := point.NewPoint3D(120, 20, 0)
-	b.DrawTriangle(point.NewPoint2D(80, 20), point.NewPoint2D(100, 40), point.NewPoint2D(120, 20), pixels)
 	dy := 40
-	b.RotateTriangle3DAroundX(p1.AddToY(dy), p2.AddToY(dy), p3.AddToY(dy), theta, focalLength, pixels)
-	b.RotateTriangle3DAroundY(p1.AddToY(dy*2), p2.AddToY(dy*2), p3.AddToY(dy*2), theta, focalLength, pixels)
-	b.RotateTriangle3DAroundZ(p1.AddToY(dy*3), p2.AddToY(dy*3), p3.AddToY(dy*3), theta, focalLength, pixels)
+	p1 := point.NewPoint2D(80, 20)
+	p2 := point.NewPoint2D(100, 40)
+	p3 := point.NewPoint2D(120, 20)
+	b.DrawPolygon(shapes.NewTriangle(p1, p2, p3), pixels)
+	b.DrawPolygon3D(shapes.NewTriangle(p1.AddToY(dy), p2.AddToY(dy), p3.AddToY(dy)).RotateAroundX(theta), focalLength, pixels)
+	b.DrawPolygon3D(shapes.NewTriangle(p1.AddToY(dy*2), p2.AddToY(dy*2), p3.AddToY(dy*2)).RotateAroundY(theta), focalLength, pixels)
+	b.DrawPolygon3D(shapes.NewTriangle(p1.AddToY(dy*3), p2.AddToY(dy*3), p3.AddToY(dy*3)).RotateAroundZ(theta), focalLength, pixels)
 
 	vertices3d := []*point.Point3D{
 		point.NewPoint3D(140, 20, 0),
@@ -117,10 +116,10 @@ func (b *Board) Draw(pixels []byte, counter, focalLength int) {
 		vertices3d3[7].AddToY(dy),
 	}
 
-	b.DrawPolygon3D(vertices3d, edges3d, focalLength, pixels)
-	b.RotatePolygon3DAroundX(vertices3d2, edges3d, -theta, focalLength, pixels)
-	b.RotatePolygon3DAroundY(vertices3d3, edges3d, -theta, focalLength, pixels)
-	b.RotatePolygon3DAroundZ(vertices3d4, edges3d, -theta, focalLength, pixels)
+	b.DrawPolygon3D(shapes.NewPolygon3D(vertices3d, edges3d), focalLength, pixels)
+	b.DrawPolygon3D(shapes.NewPolygon3D(vertices3d2, edges3d).RotateAroundX(-theta), focalLength, pixels)
+	b.DrawPolygon3D(shapes.NewPolygon3D(vertices3d3, edges3d).RotateAroundY(-theta), focalLength, pixels)
+	b.DrawPolygon3D(shapes.NewPolygon3D(vertices3d4, edges3d).RotateAroundZ(-theta), focalLength, pixels)
 }
 
 // Draw2 draws board
@@ -145,185 +144,29 @@ func (b *Board) Draw2(pixels []byte, counter, focalLength int) {
 		{6, 2}, {6, 4}, {6, 7},
 	}
 
-	// b.DrawPolygon3D(vertices3d, edges3d, focalLength, pixels)
-	// b.RotatePolygon3DAroundY(vertices3d, edges3d, 90.0*math.Pi/180.0, focalLength, pixels)
-	// b.RotatePolygon3DAroundX(vertices3d, edges3d, -theta, focalLength, pixels)
-	// b.RotatePolygon3DAroundY(vertices3d, edges3d, -theta, focalLength, pixels)
-	// b.RotatePolygon3DAroundZ(vertices3d, edges3d, -theta, focalLength, pixels)
-	b.RotatePolygon3DAroundXAndY(vertices3d, edges3d, -theta, focalLength, pixels)
-}
-
-// DrawTriangle draws a triangle
-func (b *Board) DrawTriangle(p1, p2, p3 *point.Point2D, pixels []byte) {
-	b.DrawLine(p1, p2, pixels)
-	b.DrawLine(p2, p3, pixels)
-	b.DrawLine(p3, p1, pixels)
-}
-
-// RotateTriangle rotates a triangle
-func (b *Board) RotateTriangle(p1, p2, p3 *point.Point2D, theta float64, pixels []byte) {
-	pc := point.NewPoint2D((p1.X+p2.X+p3.X)/3, (p1.Y+p2.Y+p3.Y)/3)
-	b.DrawTriangle(p1.RotatePoint(pc, theta), p2.RotatePoint(pc, theta), p3.RotatePoint(pc, theta), pixels)
-}
-
-// DrawTriangle3D draws a triangle with z axis
-func (b *Board) DrawTriangle3D(p1, p2, p3 *point.Point3D, focalLength int, pixels []byte) {
-	pc := point.NewPoint2D((p1.X+p2.X+p3.X)/3, (p1.Y+p2.Y+p3.Y)/3)
-	b.DrawLine3D(p1, p2, pc, focalLength, pixels)
-	b.DrawLine3D(p2, p3, pc, focalLength, pixels)
-	b.DrawLine3D(p3, p1, pc, focalLength, pixels)
-}
-
-// RotateTriangle3DAroundX rotates a triangle around X axis
-func (b *Board) RotateTriangle3DAroundX(p1, p2, p3 *point.Point3D, theta float64, focalLength int, pixels []byte) {
-	pc := point.NewPoint3D((p1.X+p2.X+p3.X)/3, (p1.Y+p2.Y+p3.Y)/3, (p1.Z+p2.Z+p3.Z)/3)
-	b.DrawTriangle3D(p1.RotatePoint3DAroundX(pc, theta), p2.RotatePoint3DAroundX(pc, theta), p3.RotatePoint3DAroundX(pc, theta), focalLength, pixels)
-}
-
-// RotateTriangle3DAroundY rotates a triangle around Y axis
-func (b *Board) RotateTriangle3DAroundY(p1, p2, p3 *point.Point3D, theta float64, focalLength int, pixels []byte) {
-	pc := point.NewPoint3D(p2.X, p1.Y, p1.Z)
-	b.DrawTriangle3D(p1.RotatePoint3DAroundY(pc, theta), p2.RotatePoint3DAroundY(pc, theta), p3.RotatePoint3DAroundY(pc, theta), focalLength, pixels)
-}
-
-// RotateTriangle3DAroundZ rotates a trianglearound Z axis
-func (b *Board) RotateTriangle3DAroundZ(p1, p2, p3 *point.Point3D, theta float64, focalLength int, pixels []byte) {
-	pc := point.NewPoint3D((p1.X+p2.X+p3.X)/3, (p1.Y+p2.Y+p3.Y)/3, (p1.Z+p2.Z+p3.Z)/3)
-	b.DrawTriangle3D(p1.RotatePoint3DAroundZ(pc, theta), p2.RotatePoint3DAroundZ(pc, theta), p3.RotatePoint3DAroundZ(pc, theta), focalLength, pixels)
-}
-
-// DrawQuadrangle draws a quadrangle
-func (b *Board) DrawQuadrangle(p1, p2, p3, p4 *point.Point2D, pixels []byte) {
-	b.DrawLine(p1, p2, pixels)
-	b.DrawLine(p2, p3, pixels)
-	b.DrawLine(p3, p4, pixels)
-	b.DrawLine(p4, p1, pixels)
-}
-
-// RotateQuadrangle rotates a quadrangle
-func (b *Board) RotateQuadrangle(p1, p2, p3, p4 *point.Point2D, theta float64, pixels []byte) {
-	pc := point.NewPoint2D((p1.X+p2.X+p3.X+p4.X)/4, (p1.Y+p2.Y+p3.Y+p4.Y)/4)
-	b.DrawQuadrangle(p1.RotatePoint(pc, theta), p2.RotatePoint(pc, theta), p3.RotatePoint(pc, theta), p4.RotatePoint(pc, theta), pixels)
+	b.DrawPolygon3D(shapes.NewPolygon3D(vertices3d, edges3d).RotateAroundX(-theta).RotateAroundY(-theta), focalLength, pixels)
 }
 
 // DrawPolygon draws a polygon based on vertices and edges matrices
-func (b *Board) DrawPolygon(vertices []*point.Point2D, edges [][2]int, pixels []byte) {
-	for _, edge := range edges {
-		b.DrawLine(vertices[edge[0]], vertices[edge[1]], pixels)
+func (b *Board) DrawPolygon(polygon *shapes.Polygon2D, pixels []byte) {
+	for _, edge := range polygon.Edges {
+		b.DrawLine(polygon.Vertices[edge[0]], polygon.Vertices[edge[1]], pixels)
 	}
-}
-
-// RotatePolygon rotates a polygon
-func (b *Board) RotatePolygon(vertices []*point.Point2D, edges [][2]int, theta float64, pixels []byte) {
-	var sumX, sumY int
-	lv := len(vertices)
-	for _, v := range vertices {
-		sumX += v.X
-		sumY += v.Y
-	}
-	pc := point.NewPoint2D(sumX/lv, sumY/lv)
-
-	rotatedVertices := make([]*point.Point2D, 0)
-	for _, v := range vertices {
-		rotatedVertices = append(rotatedVertices, v.RotatePoint(pc, theta))
-	}
-
-	b.DrawPolygon(rotatedVertices, edges, pixels)
 }
 
 // DrawPolygon3D draws a 3D polygon based on vertices and edges matrices
-func (b *Board) DrawPolygon3D(vertices []*point.Point3D, edges [][2]int, focalLength int, pixels []byte) {
+func (b *Board) DrawPolygon3D(polygon *shapes.Polygon3D, focalLength int, pixels []byte) {
 	var sumX, sumY int
-	lv := len(vertices)
-	for _, v := range vertices {
+	lv := len(polygon.Vertices)
+	for _, v := range polygon.Vertices {
 		sumX += v.X
 		sumY += v.Y
 	}
 
 	pc := point.NewPoint2D(sumX/lv, sumY/lv)
-	for _, edge := range edges {
-		b.DrawLine3D(vertices[edge[0]], vertices[edge[1]], pc, focalLength, pixels)
+	for _, edge := range polygon.Edges {
+		b.DrawLine3D(polygon.Vertices[edge[0]], polygon.Vertices[edge[1]], pc, focalLength, pixels)
 	}
-}
-
-// RotatePolygon3DAroundX rotates a polygon around X axis
-func (b *Board) RotatePolygon3DAroundX(vertices []*point.Point3D, edges [][2]int, theta float64, focalLength int, pixels []byte) {
-	var sumX, sumY, sumZ int
-	lv := len(vertices)
-	for _, v := range vertices {
-		sumX += v.X
-		sumY += v.Y
-		sumZ += v.Z
-	}
-
-	pc := point.NewPoint3D(sumX/lv, sumY/lv, sumZ/lv)
-
-	rotatedVertices := make([]*point.Point3D, 0)
-	for _, v := range vertices {
-		rotatedVertices = append(rotatedVertices, v.RotatePoint3DAroundX(pc, theta))
-	}
-
-	b.DrawPolygon3D(rotatedVertices, edges, focalLength, pixels)
-}
-
-// RotatePolygon3DAroundY rotates a polygon around Y axis
-func (b *Board) RotatePolygon3DAroundY(vertices []*point.Point3D, edges [][2]int, theta float64, focalLength int, pixels []byte) {
-	var sumX, sumY, sumZ int
-	lv := len(vertices)
-	for _, v := range vertices {
-		sumX += v.X
-		sumY += v.Y
-		sumZ += v.Z
-	}
-
-	pc := point.NewPoint3D(sumX/lv, sumY/lv, sumZ/lv)
-
-	rotatedVertices := make([]*point.Point3D, 0)
-	for _, v := range vertices {
-		rotatedVertices = append(rotatedVertices, v.RotatePoint3DAroundY(pc, theta))
-	}
-
-	b.DrawPolygon3D(rotatedVertices, edges, focalLength, pixels)
-}
-
-// RotatePolygon3DAroundZ rotates a polygon around Z axis
-func (b *Board) RotatePolygon3DAroundZ(vertices []*point.Point3D, edges [][2]int, theta float64, focalLength int, pixels []byte) {
-	var sumX, sumY, sumZ int
-	lv := len(vertices)
-	for _, v := range vertices {
-		sumX += v.X
-		sumY += v.Y
-		sumZ += v.Z
-	}
-
-	pc := point.NewPoint3D(sumX/lv, sumY/lv, sumZ/lv)
-
-	rotatedVertices := make([]*point.Point3D, 0)
-	for _, v := range vertices {
-		rotatedVertices = append(rotatedVertices, v.RotatePoint3DAroundZ(pc, theta))
-	}
-
-	b.DrawPolygon3D(rotatedVertices, edges, focalLength, pixels)
-}
-
-// RotatePolygon3DAroundXAndY rotates a polygon around X and Y axis
-func (b *Board) RotatePolygon3DAroundXAndY(vertices []*point.Point3D, edges [][2]int, theta float64, focalLength int, pixels []byte) {
-	var sumX, sumY, sumZ int
-	lv := len(vertices)
-	for _, v := range vertices {
-		sumX += v.X
-		sumY += v.Y
-		sumZ += v.Z
-	}
-
-	pc := point.NewPoint3D(sumX/lv, sumY/lv, sumZ/lv)
-
-	rotatedVertices := make([]*point.Point3D, 0)
-	for _, v := range vertices {
-		rotatedVertices = append(rotatedVertices, v.RotatePoint3DAroundXAndY(pc, theta))
-	}
-
-	b.DrawPolygon3D(rotatedVertices, edges, focalLength, pixels)
 }
 
 // DrawLine3DRelativeToPoint draws a line
